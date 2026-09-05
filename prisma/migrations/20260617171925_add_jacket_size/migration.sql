7b93f732a894a340a3e8748985fa3b139cceb7c2
gehadeladl <gehadeladl19@gmail.com>
2026-09-05 19:56:05 +0300

    Fix Prisma generation on Vercel build

--- prisma/migrations/20260617171925_add_jacket_size/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "CustomerMeasurement" ADD COLUMN "jacketSize" REAL;
--- prisma/migrations/20260618131850_create_inventory/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "InventoryItem" ADD COLUMN "color" TEXT;
-ALTER TABLE "InventoryItem" ADD COLUMN "material" TEXT;
-ALTER TABLE "InventoryItem" ADD COLUMN "size" TEXT;
-ALTER TABLE "InventoryItem" ADD COLUMN "style" TEXT;
